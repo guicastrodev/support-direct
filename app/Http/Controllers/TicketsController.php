b7f7ccd0d15cac16475603bb8cea2fa6255f7d11
Guilherme <guilherme@castrodev.com.br>
2023-08-11 15:28:09 -0300

    - Equiparação das categorias definidas na base com as categorias disponíveis para edição;

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -10,39 +10,39 @@ class TicketsController extends Controller
 {
     public function index()
     {
-        $userID = auth()->id();  
-        $perfil = auth()->user()->tipo;  
+        $userID = auth()->id();
+        $perfil = auth()->user()->tipo;
 
         switch ($perfil) {
             case 'cliente':
-                $tickets = Chamado::where('requerenteID', $userID )->get();
+                $tickets = Chamado::where('requerenteID', $userID)->get();
                 break;
             case 'tecnico':
-                $tickets = Chamado::where('tecnicoID', $userID )->get();
+                $tickets = Chamado::where('tecnicoID', $userID)->get();
                 break;
             case 'gestor':
-                $tickets = Chamado::where('gestorID', $userID )->get();
+                $tickets = Chamado::where('gestorID', $userID)->get();
                 break;
             default:
                 // O tipo do usuário é uma informação obrigatória!
                 break;
-        } 
-        
-        return view('tickets', compact('tickets','perfil'));
+        }
+
+        return view('tickets', compact('tickets', 'perfil'));
     }
-    
+
     public function show($id)
     {
-        $categorias = ['Rede', 'Software', 'Materiais', 'Hardware', 'Segurança'];
+        $categorias = ['Acesso', 'Conexão', 'E-mail', 'Hardware', 'Impressão', 'Materiais', 'Rede', 'Segurança', 'Servidor', 'Sistema', 'Software'];
         $prioridades = ['baixa', 'media', 'alta'];
-        $perfil = auth()->user()->tipo; 
+        $perfil = auth()->user()->tipo;
         if ($id == 'novo') {
             return view('novochamado', compact('categorias', 'prioridades'));
         } else {
             $chamado = Chamado::findOrFail($id);
             $tecnicos = User::where('tipo', 'tecnico')->get();
-            $situacoes = ['Aberto','Em análise', 'Resolvido', 'Cancelado', 'Aguardando Requerente', 'Aguardando Fornecedor'];
-            return view('chamado', compact('chamado', 'categorias', 'prioridades', 'tecnicos', 'situacoes','perfil'));
+            $situacoes = ['Aberto', 'Em análise', 'Resolvido', 'Cancelado', 'Aguardando Requerente', 'Aguardando Fornecedor'];
+            return view('chamado', compact('chamado', 'categorias', 'prioridades', 'tecnicos', 'situacoes', 'perfil'));
         }
     }
 
